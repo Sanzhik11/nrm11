0f6afa54aeccc389170306ab518c98b0473ca9f4
Sanzhik11 <sanzhikkzo111@icloud.com>
2024-12-09 15:52:57 +0600

    Add files via upload

---
+++ ekzamen/js/script.js
@@ -0,0 +1,52 @@
+let eShop = {
+    name: "MSQ",
+    phoneNumber: +77071011111,
+    obuvi: [
+        {
+            productName: "Обувь",
+            category: "MSQ",
+            price: "64 990",
+            stock: "8 дана",
+        }
+    ],
+    switter: [
+            {
+                productName: "Свиттер",
+                category: "MSQ",
+                price: "32 990",
+                stock: "8 дана",
+        }
+    ],
+    poluzamok: [
+        {
+            productName: "Полузамок",
+            category: "MSQ",
+            price: "23 990",
+            stock: "6 дана",
+        }
+    ],
+    botinka: [
+        {
+            productName: "Ботинка",
+            category: "MSQ",
+            price: "47 890",
+            stock: "9 дана",
+        }
+    ],
+    sportivka: [
+        {
+            productName: "Спортивка",
+            category: "MSQ",
+            price: "37 890",
+            stock: "5 дана", 
+        }
+    ],
+    if (eShop) {
+        let changeShopName = prompt;
+    }
+    
+}
+let sanz = eShop.name.slice(changeShopName)
+console.log(sanz, eShop);
+
+
